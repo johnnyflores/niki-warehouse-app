9036a527c517020e3682364f8f4e1368d1148b14
Johnny <johnnyflores@live.ca>
2026-02-22 00:33:15 -0500

    Added microservices

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from redis_om import get_redis_connection
+from redis_om import get_redis_connection, HashModel
 
 from dotenv import load_dotenv
 import os
@@ -32,3 +32,34 @@ redis = get_redis_connection(
     decode_responses=True
 )
 
+class Product(HashModel, index=True):
+    name: str
+    price: float
+    quantity: int
+    class Meta:
+        database = redis
+
+@app.post('/product')
+def create(product: Product):
+    return product.save()
+    
+@app.get('/product/{pk}')
+def get(pk: str):
+    return Product.get(pk)
+
+@app.get('/products')
+def get_all():
+    return [format(pk) for pk in Product.all_pks()]
+
+def format(pk: str):
+    product = Product.get(pk)
+    return {
+        'id': product.pk,
+        'name': product.name,
+        'price': product.price,
+        'quantity': product.quantity
+    }
+
+@app.delete('/product/{pk}')
+def delete(pk: str):
+    return Product.delete(pk)
