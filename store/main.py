669d10fe9f44c426e024275c0f667075500f56a7
Johnny <johnnyflores@live.ca>
2026-02-22 17:05:52 -0500

    Implement order microservice

---
+++ store/main.py
@@ -0,0 +1,84 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from redis_om import get_redis_connection, HashModel
+from dotenv import load_dotenv
+import os
+import requests
+
+load_dotenv() # take environment variables from .env.
+
+HOST = os.getenv('HOST')
+PORT = os.getenv('PORT')
+PASSWORD = os.getenv('PASSWORD')
+
+app = FastAPI()
+
+origins = [
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+redis = get_redis_connection(
+    host=HOST,
+    port=int(PORT),
+    password=PASSWORD,
+    decode_responses=True
+)
+
+class ProductOrder(HashModel):
+    product_id: str
+    quantity: int
+    class Meta:
+        database = redis
+
+class Order(HashModel, index=True):
+    product_id: str
+    price: float
+    fee: float
+    total: float
+    quantity: int
+    status: str
+    class Meta:
+        database = redis
+
+@app.post('/orders')
+def create(productOrder: ProductOrder):
+    req = requests.get(f'http://localhost:8000/product/{productOrder.product_id}')
+    product = req.json()
+    fee = product['price'] * 0.2
+
+    order = Order(
+        product_id = productOrder.product_id,
+        price = product['price'],
+        fee = fee,
+        total = product['price'] + fee,
+        quantity = productOrder.quantity,
+        status = 'pending'
+    )
+    return order.save()
+
+@app.get('/orders/{pk}')
+def get(pk: str):
+    return format(pk)
+
+@app.get('/orders')
+def get_all():
+    return [format(pk) for pk in Order.all_pks()]
+
+def format(pk: str):
+    order = Order.get(pk)
+    return {
+        'id': order.pk,
+        'product_id': order.product_id,
+        'fee': order.fee,
+        'total': order.total,
+        'quantity': order.quantity,
+        'status': order.status
+    }
